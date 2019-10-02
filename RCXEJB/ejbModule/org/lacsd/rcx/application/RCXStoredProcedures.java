4621924c360a7d7deb5991b5ea8b6d08e638cdc1
Carlos Rojas <crojas1337@gmail.com>
2019-10-02 14:48:57 -0700

    Added in more missing files

---
+++ RCXEJB/ejbModule/org/lacsd/rcx/application/RCXStoredProcedures.java
@@ -0,0 +1,23 @@
+package org.lacsd.rcx.application;
+
+/******************************************************************************
+//* Copyright (c) 2019 Sanitation Districts of Los Angeles. All Rights Reserved.
+//* Filename:		RCXStoreProcedures.java
+//* Revision: 		1.0
+//* Author:			christophersimmmons@lacsd.org
+//* Created On: 	Aug 21, 2019
+//* Modified By:    
+//* Modified On:    
+//* 
+//* Description:	
+/******************************************************************************/
+
+
+public class RCXStoredProcedures {
+	
+	//Materials
+	public static String RCXMATL001SP = "dbo.RCXMATL001SP";
+	
+	//Vendors
+	public static String RCXVEND001SP = "dbo.RCXVEND001SP";
+}
